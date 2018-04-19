0580a77ae4f9c8fffc73c38f3c6974f063dcdad4
nahid hassan <hassan35-1246@diu.edu.bd>
2018-04-20 00:10:19 +0600

    Initial commit

---
+++ admin panel/database/seeds/UserSeed.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $items = [
+            
+            ['id' => 1, 'name' => 'Admin', 'email' => 'admin@admin.com', 'password' => '$2y$10$a4dJj10Z9DST9a5P9zjk0uBZYThLoY.D/dv.to8sm3jUSJWoRoUOG', 'role_id' => 1, 'remember_token' => '',],
+
+        ];
+
+        foreach ($items as $item) {
+            \App\User::create($item);
+        }
+    }
+}
